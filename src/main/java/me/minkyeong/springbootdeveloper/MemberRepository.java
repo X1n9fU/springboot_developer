a1c7c2ef44a5c37881210875874ff15c613e7f3b
minky <minkyeong02@daum.net>
2024-07-03 00:52:53 +0900

    [feat] 05.데이터베이스 조작이 편해지는 ORM

--- src/main/java/me/minkyeong/springbootdeveloper/MemberRepository.java
+++ src/main/java/me/minkyeong/springbootdeveloper/MemberRepository.java
@@ -1,9 +1,16 @@
 package me.minkyeong.springbootdeveloper;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByName(String name);
+    //쿼리 메소드
 
+    @Query("select m from Member m where m.name = ?1")
+    Optional<Member> findByNameQuery(String name);
 }
